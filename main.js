af907cd8b37e4ba9009d53b356f4bdfee25a8f5c
Darek <dardman88@gmail.com>
2020-01-27 14:02:56 +0100

    Change way to display elements in main.js

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+function changeContent() {
+   // sections
+   const front = document.querySelector(".front");
+   const aboutMe = document.querySelector(".main__about-me");
+   const technologies = document.querySelector(".main__technologies");
+   const projects = document.querySelector(".main__projects");
+
+   // menu buttons
+   const menuList = document.querySelectorAll(".header__menu-list");
+   const start = document.querySelector(".header__start");
+   const aboutMeButton = document.querySelector(".header__about-me");
+   const technologiesButton = document.querySelector(".header__technologies");
+   const projectsButton = document.querySelector(".header__projects");
+
+   const content = [front, aboutMe, technologies, projects];
+   const contentButtons = [start, aboutMeButton, technologiesButton, projectsButton];
+   let prevContent;
+
+   menuList.forEach(element => {
+      element.addEventListener("click", function(e) {
+         // console.log(e.target);
+         // console.log(aboutMeButton);
+
+         const index = contentButtons.findIndex(item => {
+            return e.target === item;
+         });
+         // console.log(e.target === start);
+         // front.setAttribute("style", "display: none");
+         e.target !== start
+            ? content[index].setAttribute("style", "display: grid")
+            : content[index].setAttribute("style", "display: flex");
+         prevContent = content.filter(item => {
+            return item !== content[index];
+         });
+         prevContent.forEach(item => {
+            item.setAttribute("style", "display: none");
+         });
+         // console.log(e.target === aboutMeButton);
+
+         // console.log(front.innerHTML);
+      });
+   });
+}
+
+changeContent();
+
 // function openText() {
 //    const techSection = document.querySelectorAll(".main__open");
 //    const techImage = document.querySelectorAll(".main__techImg");
@@ -20,16 +66,16 @@
 
 //////// Smooth scroll
 
-$(".header__menu a").smoothScroll({
-   offset: -50,
-   afterScroll: function() {
-      $(this)
-         .closest(".header__menu")
-         .find("a")
-         .removeClass("active");
-      $(this).addClass("active");
-   }
-});
+// $(".header__menu a").smoothScroll({
+//    offset: -50,
+//    afterScroll: function() {
+//       $(this)
+//          .closest(".header__menu")
+//          .find("a")
+//          .removeClass("active");
+//       $(this).addClass("active");
+//    }
+// });
 
 // $(".button-top a").smoothScroll({
 //    offset: 0,
